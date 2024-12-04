0a6bef3e739197ceed4a8331f8349b166ad18dee
mike <mike@herebox.org>
2024-12-04 15:12:50 -0800

    3.2 test working regex

--- 3.2/main.py
+++ 3.2/main.py
@@ -10,8 +10,9 @@ class Aoc:
         self.testing = testing
         self.lines = []
         self.result = 0
+        self.enabled = True
 
-        self.pattern = re.compile('mul\((\d\d?\d?),(\d\d?\d?)\)')
+        self.pattern = re.compile("mul\((\d\d?\d?),(\d\d?\d?)\)|(do\(\))|(don't\(\))")
 
         if testing:
             input = os.path.dirname(__file__) + '/test'
@@ -29,11 +30,21 @@ class Aoc:
             self.mul_line(line)
 
     def mul_line(self, line):
-        muls = self.pattern.findall(line)
-        print(muls)        
-        
-        for pair in muls:
-            self.result += int(pair[0]) * int(pair[1])
+        matches = self.pattern.findall(line)
+        print(matches)
+
+        for match in matches:
+            if match[3] == "don't()":
+                self.enabled = False
+                print("don't()")
+                continue
+            if match[2] == "do()":
+                self.enabled = True
+                print("do()")
+                continue
+            if self.enabled:
+                self.result += int(match[0]) * int(match[1])
+            
             print(self.result)
 
         # return self.result
