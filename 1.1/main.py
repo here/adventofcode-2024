cd89e956ecc050a088d84c59babb7fa57d678b5e
mike <mike@herebox.org>
2024-12-04 14:12:25 -0800

    1.1 test working

--- 1.1/main.py
+++ 1.1/main.py
@@ -8,3 +8,26 @@ with open('in') as file:
         lines.append(line)
 
 print(len(lines))
+
+spread = []
+left = []
+right = []
+
+
+for line in lines:
+    l, r = line.split()
+    left.append(l)
+    right.append(r)
+
+left = sorted(left)
+right = sorted(right)
+
+print(left)
+print(right)
+
+sumdiff = 0
+
+for i, l in enumerate(left):
+    sumdiff += abs(int(left[i])-int(right[i]))
+
+print(sumdiff)
